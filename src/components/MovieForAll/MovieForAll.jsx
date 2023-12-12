8ef6c741b2e8987f18164836193227848d4a4dc9
mixishop-ui <nguyenhoi13041999@gmail.com>
2023-12-13 00:29:59 +0700

    update message loading

--- src/components/MovieForAll/MovieForAll.jsx
+++ src/components/MovieForAll/MovieForAll.jsx
@@ -7,16 +7,18 @@ import MovieList from "../MovieList/MovieList";
 
 const MovieForAll = () => {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchMovies = async () => {
+      setLoading(true);
       const response = await fetch(
         "https://movie-backend-8506.onrender.com/api/movies"
       );
       const dataMovies = await response.json();
       setMovies(dataMovies.data);
+      setLoading(false);
     };
-
     fetchMovies();
   }, []);
 
@@ -36,39 +38,45 @@ const MovieForAll = () => {
     setShowMovie(true);
   };
   return (
-    <div>
-      <Navbar />
-      <section className={classes.container}>
-        <span className={classes.title}>
-          <MovieList />
-        </span>
-      </section>
-      <section className={classes.content}>
-        <div className={classes.item}>
-          {/* kiểm tra xem có dữ liệu hay không có thì hiển thị ra giao diện */}
-          {movies?.map((movie, index) => (
-            <section key={index}>
-              {/* kiểm tra movies các img movies hợp lệ không thì mới được chọn để render ra giao diện */}
-              {movie.backdrop_path && (
-                <img
-                  className={classes.movieImg}
-                  key={movie.id}
-                  src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-                  alt="imgFilms"
-                  onClick={() => {
-                    setImgInFor(movie);
-                    showDetailMovieHandler();
-                  }}
-                />
-              )}
-            </section>
-          ))}
+    <>
+      {loading ? (
+        <h1>Loading please wait</h1>
+      ) : (
+        <div>
+          <Navbar />
+          <section className={classes.container}>
+            <span className={classes.title}>
+              <MovieList />
+            </span>
+          </section>
+          <section className={classes.content}>
+            <div className={classes.item}>
+              {/* kiểm tra xem có dữ liệu hay không có thì hiển thị ra giao diện */}
+              {movies?.map((movie, index) => (
+                <section key={index}>
+                  {/* kiểm tra movies các img movies hợp lệ không thì mới được chọn để render ra giao diện */}
+                  {movie.backdrop_path && (
+                    <img
+                      className={classes.movieImg}
+                      key={movie.id}
+                      src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                      alt="imgFilms"
+                      onClick={() => {
+                        setImgInFor(movie);
+                        showDetailMovieHandler();
+                      }}
+                    />
+                  )}
+                </section>
+              ))}
+            </div>
+          </section>
+          {showMovie && (
+            <MovieDetail imgInFor={imgInFor} onClose={hideDetailMovieHandler} />
+          )}
         </div>
-      </section>
-      {showMovie && (
-        <MovieDetail imgInFor={imgInFor} onClose={hideDetailMovieHandler} />
       )}
-    </div>
+    </>
   );
 };
 
